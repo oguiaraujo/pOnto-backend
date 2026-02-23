635f22a036e49f9281f08ba01d40d66361da0a0a
Guilherme Araújo <guilherme.araujo.702@ufrn.edu.br>
2026-02-23 16:54:35 -0300

    feat: adiciona as urls da api

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('bolsistas/', views.lista_bolsistas, name='lista_bolsistas'),
+    path('bolsistas/<int:pk>/', views.busca_bolsista, name='busca_bolsista'),
+    path('bolsistas/<int:pk>/ponto/', views.ponto_bolsista, name='ponto_bolsista'),
+    path('bolsistas/<int:pk>/sessoes/', views.sessoes_bolsista, name='sessoes_bolsista')
+]
